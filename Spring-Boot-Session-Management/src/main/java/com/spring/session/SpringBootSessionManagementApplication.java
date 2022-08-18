1afb2f12384240023e3ac644fd82d19335578d82
Manorajan Jena <manoranjan.jena@rapidsoft.co.in>
2022-08-18 12:13:44 +0530

    @Commit

---
+++ Spring-Boot-Session-Management/src/main/java/com/spring/session/SpringBootSessionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.spring.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionManagementApplication.class, args);
+	}
+
+}
